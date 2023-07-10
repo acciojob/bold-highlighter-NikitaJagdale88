52456d618a7e480c565deca0874c36abab598967
Nikita Jagdale <128221436+NikitaJagdale88@users.noreply.github.com>
2023-07-10 20:57:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 function highlight() {
-    //Write your code here
+    var boldWords = document.querySelectorAll("strong");
 
-
+    for (var i = 0; i < boldWords.length; i++) {
+        boldWords[i].style.color = "green";
+    }
 }
+
+function return_normal() {
+    var boldWords = document.querySelectorAll("strong");
+
+    for (var i = 0; i < boldWords.length; i++) {
+        boldWords[i].style.color = "black";
+    }
+}
+
+var highlightLink = document.getElementById("highlightLink");
+
+highlightLink.addEventListener("mouseover", highlight);
+highlightLink.addEventListener("mouseout", return_normal);
 
 
 function return_normal() {
